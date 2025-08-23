07f3c097486b854189d142a69fd3e45167577d2f
swigal <hello@samuelwigal.com>
2025-08-23 17:11:40 -0400

    Added Old and New Worship Vol 2

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 /* Place your JavaScript in this file */
+function changeOldNewVolOneAudio(newAudioLink, currentTitle) {
+        const audioPlayer = document.getElementById('oldNewVolOne')
+        const audioSource = document.getElementById('oldNewVolOneSrc')
+        const playingTitle = document.getElementById('oldNewVolOneCurrentPlay')
+
+        playingTitle.innerHTML = currentTitle
+        audioSource.src = newAudioLink
+        audioPlayer.load()
+        audioPlayer.play()
+       }
+
+function changeOldNewVolTwoAudio(newAudioLink, currentTitle) {
+        const audioPlayer = document.getElementById('oldNewVolTwo')
+        const audioSource = document.getElementById('oldNewVolTwoSrc')
+        const playingTitle = document.getElementById('oldNewVolTwoCurrentPlay')
+
+        playingTitle.innerHTML = currentTitle
+        audioSource.src = newAudioLink
+        audioPlayer.load()
+        audioPlayer.play()
+       }
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
